c2f7ac3a7f088c05197f9a51d9d919d1b6630abe
ShayanHaddadian <shayanhaddadian849@yahoo.com>
2016-03-19 17:06:21 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {100142213},--Sudo users
+    sudo_users = {100142213,76798631},--Sudo users
     disabled_channels = {},
     moderation = {100142213},
     about_text = [[Teleseed v2 - Open Source
